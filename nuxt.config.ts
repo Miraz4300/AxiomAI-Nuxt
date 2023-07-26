cb6b1714407757d48c235e0ce1d23a4edff83f3b
Miraz Hossain <miraz4300@gmail.com>
2023-07-26 10:05:57 +0000

    axiomai-nuxt template

---
+++ nuxt.config.ts
@@ -0,0 +1,23 @@
+import Components from 'unplugin-vue-components/vite'
+import { NaiveUiResolver } from 'unplugin-vue-components/resolvers'
+
+export default defineNuxtConfig({
+  build: {
+    transpile: process.env.NODE_ENV === 'production' ? ['naive-ui', 'vueuc', '@css-render/vue3-ssr', '@juggle/resize-observer', 'date-fns', '@css-render/plugin-bem'] : ['@juggle/resize-observer'],
+  },
+  vite: {
+    plugins: [
+      Components({
+        dts: true,
+        resolvers: [NaiveUiResolver()], // Automatically register all components in the `components` directory
+      }),
+    ],
+  },
+  devtools: { enabled: true },
+  modules: [
+    '@nuxtjs/eslint-module',
+    '@nuxtjs/tailwindcss',
+    '@nuxtjs/i18n',
+    'nuxt-icon',
+  ],
+})
---
+++ tailwind.config.js
@@ -0,0 +1,30 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  darkMode: 'class',
+  content: [
+    './index.html',
+    './src/**/*.{vue,js,ts,jsx,tsx}',
+  ],
+  theme: {
+    fontFamily: {
+      logo: ['Consolas', 'sans-serif'],
+      header: ['Arial', 'sans-serif'],
+    },
+    extend: {
+      animation: {
+        blink: 'blink 1.2s infinite steps(1, start)',
+      },
+      backgroundImage: {
+        'sign-in-background': 'url(\'/images/sign-in-background.svg\')',
+        'sign-in-background-dark': 'url(\'/images/sign-in-background-dark.svg\')',
+      },
+      keyframes: {
+        blink: {
+          '0%, 100%': { 'background-color': 'currentColor' },
+          '50%': { 'background-color': 'transparent' },
+        },
+      },
+    },
+  },
+  plugins: [],
+}
